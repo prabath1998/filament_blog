913d091324369ec149bdfa26d58cff5e6aebd9b3
prabath1998 <prabhathudayanga1998@gmail.com>
2022-10-13 21:58:43 +0530

    thumbnails uploaded

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -10,12 +10,17 @@ use Filament\Forms;
 use Filament\Forms\Components\Card;
 use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
 use Filament\Tables;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\SpatieMediaLibraryImageColumn;
+use Filament\Tables\Columns\TextColumn;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -39,6 +44,7 @@ class PostResource extends Resource
                         })->required(),
 
                     TextInput::make('slug')->required(),
+                    SpatieMediaLibraryFileUpload::make('thumbnail')->collection('posts'),
                     RichEditor::make('content'),
                     Toggle::make('is_published')
                 ])
@@ -49,7 +55,11 @@ class PostResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('id')->sortable(),
+                TextColumn::make('title')->limit(50)->sortable(),
+                TextColumn::make('slug')->limit(50),
+                IconColumn::make('is_published'),
+                SpatieMediaLibraryImageColumn::make('thumbnail')->collection('posts'),
             ])
             ->filters([
                 //
